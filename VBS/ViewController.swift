f24898796676f189b91147c66e8e18209ef5bdea
bseaverclc <bseaver@d155.org>
2023-11-22 17:19:08 -0600

    Added kill pct for both home and away

--- VBS/ViewController.swift
+++ VBS/ViewController.swift
@@ -9,14 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HomeButtonPlain: UIButton!
     @IBOutlet weak var HomeButton: UIButton!
+    @IBOutlet weak var HomeView: UIView!
+    
+    @IBOutlet weak var awayView: UIView!
+    
+    @IBOutlet weak var AwayButtonPlain: UIButton!
+    @IBOutlet weak var AwayButton: UIButton!
+    
     var homeKills = 0
     var homeBlocks = 0
     var homeScore = 0
     var awayScore = 0
-    var homeStats = [0,0,0,0,0,0]
-    var awayStats = [0,0,0,0,0,0]
+    var homeStats = [0,0,0,0,0,0,0,0]
+    var awayStats = [0,0,0,0,0,0,0,0]
+   
+    @IBOutlet weak var homeKillPctLabel: UILabel!
     
+    @IBOutlet weak var homeSrvAvgLabel: UILabel!
+    
+    @IBOutlet weak var awayKillPctLabel: UILabel!
+    
+    @IBOutlet weak var awaySrvAvgLabel: UILabel!
+    
+    @IBOutlet var homeStatsLabels: [UILabel]!
+    
+    @IBOutlet var awayStatsLabels: [UILabel]!
     
     
     @IBOutlet weak var OuterStackView: UIStackView!
@@ -39,20 +58,25 @@ class ViewController: UIViewController {
         awaySidePane.isHidden = true
         homeStatsPane.isHidden = true
         awayStatsPane.isHidden = true
+        HomeView.isHidden = true
+        awayView.isHidden = true
+        HomeButton.isHidden = true
+        AwayButton.isHidden = true
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        //OuterStackView.distribution = .fillEqually
-      // HomeButton.showsMenuAsPrimaryAction = true
-          //HomeButton.changesSelectionAsPrimaryAction = true
+      //  OuterStackView.distribution = .fillEqually
+      // HomeButton.showsMenuAsPrimaryAction = false
+       // AwayButton.showsMenuAsPrimaryAction = false
+         //HomeButton.changesSelectionAsPrimaryAction = true
         
         let homeClosure = {(action: UIAction) in
             var selection = action.title
             self.homeAction(selected: selection)
-                
-            
+                print("homeclosure")
+
            }
         HomeButton.menu = UIMenu(children: [
                 UIAction(title: "Kill", state: .on, handler: homeClosure),
@@ -61,6 +85,21 @@ class ViewController: UIViewController {
                 UIAction(title: "Opponent Attk Err", handler: homeClosure),
                 UIAction(title: "Opponent Srv Err", handler: homeClosure),
                 UIAction(title: "Opp Other Err", handler: homeClosure),
+            ])
+        
+        let awayClosure = {(action: UIAction) in
+            var selection = action.title
+            self.awayAction(selected: selection)
+                print("awayclosure")
+
+           }
+        AwayButton.menu = UIMenu(children: [
+                UIAction(title: "Kill", state: .on, handler: awayClosure),
+                UIAction(title: "Block", handler: awayClosure),
+                UIAction(title: "Ace", handler: awayClosure),
+                UIAction(title: "Opponent Attk Err", handler: awayClosure),
+                UIAction(title: "Opponent Srv Err", handler: awayClosure),
+                UIAction(title: "Opp Other Err", handler: awayClosure),
             ])
     }
     
@@ -73,17 +112,84 @@ class ViewController: UIViewController {
         switch selected {
         case "Kill":
             homeStats[0]+=1
+            homeStats[6]+=1
         case "Block":
             homeStats[1]+=1
+            awayStats[3]+=1
+            awayStats[6]+=1
         case "Ace":
             homeStats[2]+=1
+        case "Opponent Attk Err":
+            awayStats[3]+=1
+            awayStats[6]+=1
+        case "Opponent Srv Err":
+            awayStats[4]+=1
+        case "Opp Other Err":
+            awayStats[5]+=1
         default:
             print("nothing")
         }
+        updateStats()
+        
     }
     
+    func awayAction(selected: String){
+        awayScore+=1
+        AwayButton.setTitle("\(awayScore)", for: .normal)
+        AwayButton.setTitle("\(awayScore)", for: .selected)
+        
+        switch selected {
+        case "Kill":
+            awayStats[0]+=1
+            awayStats[6]+=1
+        case "Block":
+            awayStats[1]+=1
+            homeStats[3]+=1
+            homeStats[6]+=1
+        case "Ace":
+            awayStats[2]+=1
+        case "Opponent Attk Err":
+            homeStats[3]+=1
+            homeStats[6]+=1
+        case "Opponent Srv Err":
+            homeStats[4]+=1
+        case "Opp Other Err":
+            homeStats[5]+=1
+        default:
+            print("nothing")
+        }
+        updateStats()
+        
+    }
+    
+    
+    
     func updateStats(){
-       
+        for i in 0 ..< homeStatsLabels.count{
+            homeStatsLabels[i].text = "\(homeStats[i])"
+        }
+        
+        for i in 0 ..< awayStatsLabels.count{
+            awayStatsLabels[i].text = "\(awayStats[i])"
+        }
+        
+        if homeStats[6] != 0{
+            var homePercent = (Double(homeStats[0] - homeStats[3]))/Double(homeStats[6])
+            var homePercentString = String(format: "%.3f", homePercent)
+            homeKillPctLabel.text = homePercentString
+        }
+        else{
+            homeKillPctLabel.text = "0.000"
+        }
+        
+        if awayStats[6] != 0{
+            var awayPercent = (Double(awayStats[0] - awayStats[3]))/Double(awayStats[6])
+            var awayPercentString = String(format: "%.3f", awayPercent)
+            awayKillPctLabel.text = awayPercentString
+        }
+        else{
+            awayKillPctLabel.text = "0.000"
+        }
     }
     
 
@@ -96,17 +202,66 @@ class ViewController: UIViewController {
             awaySidePane.isHidden = true
             homeStatsPane.isHidden = true
             awayStatsPane.isHidden = true
+            HomeView.isHidden = true
+            awayView.isHidden = true
+            HomeButton.isHidden = true
+            AwayButton.isHidden = true
+            HomeButtonPlain.isHidden = false
+            AwayButtonPlain.isHidden = false
+            HomeButtonPlain.setTitle("\(homeScore)", for: .normal)
+            AwayButtonPlain.setTitle("\(awayScore)", for: .normal)
         case 1:
+            HomeButtonPlain.isHidden = true
+            AwayButtonPlain.isHidden = true
+            HomeView.isHidden = false
+            awayView.isHidden = false
+            HomeButton.isHidden = false
+            AwayButton.isHidden = false
+            HomeButton.showsMenuAsPrimaryAction = true
+            HomeButton.setTitle("\(homeScore)", for: .normal)
+            
+            AwayButton.showsMenuAsPrimaryAction = true
+            AwayButton.setTitle("\(awayScore)", for: .normal)
+            
             homeSidePane.isHidden = true
             awaySidePane.isHidden = true
             homeStatsPane.isHidden = false
             awayStatsPane.isHidden = false
-        default:
+        default :
+            HomeButtonPlain.isHidden = true
+            AwayButtonPlain.isHidden = true
+            HomeView.isHidden = false
+            awayView.isHidden = false
+            HomeButton.isHidden = false
+            AwayButton.isHidden = false
+            HomeButton.showsMenuAsPrimaryAction = true
+            HomeButton.setTitle("\(homeScore)", for: .normal)
+            AwayButton.showsMenuAsPrimaryAction = true
+            AwayButton.setTitle("\(awayScore)", for: .normal)
+            
             homeSidePane.isHidden = false
             awaySidePane.isHidden = false
             homeStatsPane.isHidden = false
             awayStatsPane.isHidden = false
+        
+            
         }
+    }
+    
+    
+    
+    @IBAction func homeScoreAction(_ sender: UIButton) {
+        print("homeScoreAction")
+        homeScore += 1
+        HomeButtonPlain.setTitle("\(homeScore)", for: .normal)
+        HomeButtonPlain.setTitle("\(homeScore)", for: .selected)
+    }
+    
+    @IBAction func awayScoreAction(_ sender: Any) {
+        print("awayScoreAction")
+        awayScore += 1
+        AwayButtonPlain.setTitle("\(awayScore)", for: .normal)
+        AwayButtonPlain.setTitle("\(awayScore)", for: .selected)
     }
     
     
@@ -118,7 +273,13 @@ class ViewController: UIViewController {
         let yes = UIAlertAction(title: "Yes", style: .default) { action in
             self.homeScore = 0
             self.awayScore = 0
+            self.homeStats = [0,0,0,0,0,0,0,0]
+            self.awayStats = [0,0,0,0,0,0,0,0]
+            self.updateStats()
             self.HomeButton.setTitle("\(self.homeScore)", for: .normal)
+            self.HomeButtonPlain.setTitle("\(self.homeScore)", for: .normal)
+            self.AwayButton.setTitle("\(self.homeScore)", for: .normal)
+            self.AwayButtonPlain.setTitle("\(self.homeScore)", for: .normal)
             
         }
         let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
@@ -128,6 +289,28 @@ class ViewController: UIViewController {
         present(alert, animated: true)
     }
     
+    
+    @IBAction func homeAttackAction(_ sender: UIButton) {
+        homeStats[6]+=1
+        updateStats()
+        
+    }
+    
+    @IBAction func homeDigAction(_ sender: UIButton) {
+        homeStats[7]+=1
+        updateStats()
+    }
+    
+    
+    @IBAction func awayAttackAction(_ sender: UIButton) {
+        awayStats[6]+=1
+        updateStats()
+    }
+    
 
+    @IBAction func awayDigsAction(_ sender: UIButton) {
+        awayStats[7]+=1
+        updateStats()
+    }
 }
 
