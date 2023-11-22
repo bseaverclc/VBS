d7cede710876f9048135b01c61efd8ba76b4bc3f
bseaverclc <bseaver@d155.org>
2023-11-22 13:02:21 -0600

    updated ui

--- VBS/ViewController.swift
+++ VBS/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     var homeBlocks = 0
     var homeScore = 0
     var awayScore = 0
+    var homeStats = [0,0,0,0,0,0]
+    var awayStats = [0,0,0,0,0,0]
+    
+    
     
     @IBOutlet weak var OuterStackView: UIStackView!
     
@@ -23,12 +27,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var awaySidePane: UIStackView!
     
+    @IBOutlet weak var homeStatsPane: UIStackView!
+    
+    @IBOutlet weak var awayStatsPane: UIStackView!
+    
     override func viewWillAppear(_ animated: Bool) {
        
         //centerPanel.removeConstraints(centerPanel.constraints)
         //centerPanel.widthAnchor.constraint(equalTo: OuterStackView.widthAnchor, multiplier: 1.0).isActive = true
         homeSidePane.isHidden = true
         awaySidePane.isHidden = true
+        homeStatsPane.isHidden = true
+        awayStatsPane.isHidden = true
     }
     
     override func viewDidLoad() {
@@ -39,30 +49,85 @@ class ViewController: UIViewController {
           //HomeButton.changesSelectionAsPrimaryAction = true
         
         let homeClosure = {(action: UIAction) in
-            self.homeScore+=1
-            self.HomeButton.setTitle("\(self.homeScore)", for: .normal)
-            self.HomeButton.setTitle("\(self.homeScore)", for: .selected)
             var selection = action.title
-            switch selection {
-            case "Kill":
-                self.homeKills+=1
-            case "Block":
-                self.homeBlocks+=1
-            default:
-                print("nothing")
-            }
+            self.homeAction(selected: selection)
                 
             
            }
         HomeButton.menu = UIMenu(children: [
                 UIAction(title: "Kill", state: .on, handler: homeClosure),
                 UIAction(title: "Block", handler: homeClosure),
-              
+                UIAction(title: "Ace", handler: homeClosure),
+                UIAction(title: "Opponent Attk Err", handler: homeClosure),
+                UIAction(title: "Opponent Srv Err", handler: homeClosure),
+                UIAction(title: "Opp Other Err", handler: homeClosure),
             ])
     }
     
 
+    func homeAction(selected: String){
+        homeScore+=1
+        HomeButton.setTitle("\(homeScore)", for: .normal)
+        HomeButton.setTitle("\(homeScore)", for: .selected)
+        
+        switch selected {
+        case "Kill":
+            homeStats[0]+=1
+        case "Block":
+            homeStats[1]+=1
+        case "Ace":
+            homeStats[2]+=1
+        default:
+            print("nothing")
+        }
+    }
+    
+    func updateStats(){
+       
+    }
+    
 
+    @IBAction func segmentedAction(_ sender: UISegmentedControl) {
+        var select = sender.selectedSegmentIndex
+        
+        switch select {
+        case 0:
+            homeSidePane.isHidden = true
+            awaySidePane.isHidden = true
+            homeStatsPane.isHidden = true
+            awayStatsPane.isHidden = true
+        case 1:
+            homeSidePane.isHidden = true
+            awaySidePane.isHidden = true
+            homeStatsPane.isHidden = false
+            awayStatsPane.isHidden = false
+        default:
+            homeSidePane.isHidden = false
+            awaySidePane.isHidden = false
+            homeStatsPane.isHidden = false
+            awayStatsPane.isHidden = false
+        }
+    }
+    
+    
+    
+    @IBAction func resetAction(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "Warning!", message: "Do you want to clear out all scores and stats?", preferredStyle: .alert)
+        
+        let yes = UIAlertAction(title: "Yes", style: .default) { action in
+            self.homeScore = 0
+            self.awayScore = 0
+            self.HomeButton.setTitle("\(self.homeScore)", for: .normal)
+            
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alert.addAction(yes)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
+    
 
 }
 
